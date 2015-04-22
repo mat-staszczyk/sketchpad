936be24894cb24f3e14cf364e79cb925c1813f68
Mateusz Staszczyk <kwiatkiwoknie@gmail.com>
2015-04-22 13:36:57 +0200

    Add function which set size of sketchpad

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var mbWidth = parseFloat($('.magic-box').css('width'));
 var mbHeight = parseFloat($('.magic-box').css('height'));
-var rows = 32;
-var columns = 32; 
+var rows = 16;
+var columns = 16; 
 var startColor = "white";
 var usedColor = "black";
 
@@ -13,15 +13,38 @@ var setFields = function (r, c) {
 	$('.magic-field').css('height', boxHeight);
 }
 
-$(document).ready(function() {
+var createBox = function (rows, columns) {
+	$('div.magic-field').remove();
 	var i;
 	for (i = 0; i < columns * rows; i++) {
 		$('.magic-box').append('<div class="magic-field"></div>');
 	}
+}
+
+var paintFields = function () {
 	$('.magic-field').css('background-color', startColor);
 	$('div.magic-field').mouseenter(function() {
 		$(this).css('background-color', usedColor);
 	});
+}
+
+var userFields = function () {
+	var r = prompt("Specify the number of rows:");
+	if (isNaN(r)) {
+		alert("The value must be a number, mate!");
+		return false;
+	} else if (r < 1 || r > 64) {
+		alert("The value must be a number between 1 and 64.");
+		return false;
+	}
+	createBox(r, r);
+	paintFields();
+	setFields(r, r);
+}
+
+$(document).ready(function() {
+	createBox(rows, columns);
+	paintFields();
 	setFields(rows, columns);
 })
 
