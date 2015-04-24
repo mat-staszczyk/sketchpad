ee09b143eb07ed63a6874cb1a0dd82b681542fbe
Mateusz Staszczyk <kwiatkiwoknie@gmail.com>
2015-04-24 09:55:35 +0200

    Last used color function and clearing box bug fixed

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var startColor = "#C9C9C9";
 var usedColor = "black";
 var clicked = false;
 var rand = false;
+var temp;
 
 var setFields = function (r, c) {
 	var boxWidth = (mbWidth / c) + 'px';
@@ -21,10 +22,10 @@ var createBox = function (rows, columns) {
 	for (i = 0; i < boxes; i++) {
 		$('.magic-box').append('<div class="magic-field"></div>');
 	}
+	$('.magic-field').css('background-color', startColor);
 }
 
 var paintFields = function (color) {
-	$('.magic-field').css('background-color', startColor);
   	$('.magic-box').on('mouseover', 'div', function() {
         if(clicked) {
         	if (rand) {
@@ -38,12 +39,12 @@ var paintFields = function (color) {
 }
 
 var userFields = function () {
-	var r = prompt("Specify the number of rows (1-90):\n(Warning: Large values could be really slow.)");
+	var r = prompt("Specify the number of rows (1-64):\n(Warning: Large values could be really slow.)");
 	if (isNaN(r)) {
 		alert("The value must be a number, mate!");
 		return false;
-	} else if (r < 1 || r > 100) {
-		alert("The value must be a number between 1 and 90.");
+	} else if (r < 1 || r > 64) {
+		alert("The value must be a number between 1 and 64.");
 		return false;
 	}
 	createBox(r, r);
@@ -82,7 +83,7 @@ $(document).ready(function() {
 		} else {
 			rand = false;
 			$(this).removeClass('active');
-			paintFields("black");
+			paintFields(temp);
 		}
 	});
 
